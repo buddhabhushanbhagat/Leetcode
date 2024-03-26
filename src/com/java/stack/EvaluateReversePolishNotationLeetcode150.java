b8c1a575470d7a5cd4015ee813c50ab65166e765
bhushan <bhagatbuddhabhushan@gmail.com>
2024-03-27 01:16:38 +0530

    updated stack leetcode solution

--- src/com/java/stack/EvaluateReversePolishNotationLeetcode150.java
+++ src/com/java/stack/EvaluateReversePolishNotationLeetcode150.java
@@ -6,7 +6,7 @@ public class EvaluateReversePolishNotationLeetcode150 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String[] tokens = { "4"};
+		String[] tokens = { "10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+" };
 		int result = evalRPN(tokens);
 		System.out.println(result);
 	}
@@ -15,43 +15,47 @@ public class EvaluateReversePolishNotationLeetcode150 {
 		int result = 0;
 		Stack<Integer> intStack = new Stack<>();
 		for (String s : tokens) {
-			switch (s) {
-			case "+": {
+			if (s.equals("+") || s.equals("-") || s.equals("*") || s.equals("/")) {
 				int second = intStack.pop();
 				int first = intStack.pop();
-				result = first + second;
+				result = solve(first, second, s);
 				intStack.add(result);
-				break;
-			}
-			case "-": {
-				int second = intStack.pop();
-				int first = intStack.pop();
-				result = first - second;
-				intStack.add(result);
-				break;
-			}
-			case "*": {
-				int second = intStack.pop();
-				int first = intStack.pop();
-				result = first * second;
-				intStack.add(result);
-				break;
-			}
-			case "/": {
-				int second = intStack.pop();
-				int first = intStack.pop();
-				result = first / second;
-				intStack.add(result);
-				break;
-			}
-		
-			default:
+
+			} else {
 				intStack.add(Integer.parseInt(s));
 			}
 		}
-
 		return intStack.peek();
 
+	}
+
+	private static int solve(int first, int second, String s) {
+		// TODO Auto-generated method stub
+		int result = 0;
+		switch (s) {
+		case "+": {
+
+			result = first + second;
+			break;
+		}
+		case "-": {
+
+			result = first - second;
+			break;
+		}
+		case "*": {
+
+			result = first * second;
+			break;
+		}
+		case "/": {
+
+			result = first / second;
+			break;
+		}
+
+		}
+		return result;
 	}
 
 }
