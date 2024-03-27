7c03ee7343f66a0a01d209d32a5e9ed7f11024c8
bhushan <bhagatbuddhabhushan@gmail.com>
2024-03-27 10:00:06 +0530

    Added ascending case in Treeset

--- src/com/java/collection/InsertInTreeSetDesc.java
+++ src/com/java/collection/InsertInTreeSetDesc.java
@@ -7,16 +7,27 @@ public class InsertInTreeSetDesc {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Comparator<String> c = (a,b) -> {
-			return -b.compareTo(a);
+		TreeSet<String> ts1 = new TreeSet<>();
+		ts1.add("bhushan");
+		ts1.add("shubham");
+		ts1.add("nikhil");
+		ts1.add("aman");
+
+		System.out.println(ts1);
+
+/////////////////////////////////////////////////////////
+
+		Comparator<String> c = (a, b) -> {
+			return b.compareTo(a);
 		};
-		TreeSet<String> ts = new TreeSet<>(c);
-	ts.add("bhushan");
-	ts.add("shubham");
-	ts.add("nikhil");
-	ts.add("aman");
-	
-		System.out.println(ts);
+		TreeSet<String> ts2 = new TreeSet<>(c);
+		ts2.add("bhushan");
+		ts2.add("shubham");
+		ts2.add("nikhil");
+		ts2.add("aman");
+
+		System.out.println(ts2);
+
 	}
 
 }
---
+++ src/com/java/linkedlist/REverseLinkedListLeetcode206.java
@@ -0,0 +1,10 @@
+package com.java.linkedlist;
+
+public class REverseLinkedListLeetcode206 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
