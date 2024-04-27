e3263cbb9750e24f12ebc2b8b1ca321b55cf57c8
bhushan <bhagatbuddhabhushan@gmail.com>
2024-04-27 15:54:39 +0530

    Updated first unique char from string leetcode

--- src/com/java/string/FirstUniqueCharacterStringLeetcode387.java
+++ src/com/java/string/FirstUniqueCharacterStringLeetcode387.java
@@ -1,9 +1,7 @@
 package com.java.string;
 
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 public class FirstUniqueCharacterStringLeetcode387 {
 
@@ -13,32 +11,30 @@ public class FirstUniqueCharacterStringLeetcode387 {
 		int firstUniqCharIndex = firstUniqChar(s);
 		System.out.println("firstUniqCharIndex :" + firstUniqCharIndex);
 
-		int NotValidFirstUniqCharIndex = NotValidFirstUniqChar(s);
-		System.out.println("NotValidFirstUniqCharIndex :" + NotValidFirstUniqCharIndex);
+		int FirstUniqCharIndex = firstUniqChar2(s);
+		System.out.println("NotValidFirstUniqCharIndex :" + FirstUniqCharIndex);
 	}
 
 	private static int firstUniqChar(String s) {
 		// TODO Auto-generated method stub
-		int[] arr = new int[26];   
-		s=s.toLowerCase();
-		for(char ch:s.toCharArray()) {
-			int index = ch-97;
+		int[] arr = new int[26];
+		s = s.toLowerCase();
+		for (char ch : s.toCharArray()) {
+			int index = ch - 97;
 			arr[index]++;
 		}
-		for(char ch:s.toCharArray()) {
-			int index = ch-97;
-			if(arr[index] == 1) {
+		for (char ch : s.toCharArray()) {
+			int index = ch - 97;
+			if (arr[index] == 1) {
 				return s.indexOf(ch);
 			}
 		}
 		return -1;
 	}
 
-// It will not give correct first unique index, hashmap does not maintain order
-	private static int NotValidFirstUniqChar(String s) {
+	private static int firstUniqChar2(String s) {
 		// TODO Auto-generated method stub
-		Map<Character, Integer> map = new HashMap();
-		char uniqueChar = 'a';
+		Map<Character, Integer> map = new HashMap<>();
 		for (char ch : s.toCharArray()) {
 			if (!map.containsKey(ch)) {
 				map.put(ch, 1);
@@ -46,11 +42,11 @@ public class FirstUniqueCharacterStringLeetcode387 {
 				map.put(ch, map.get(ch) + 1);
 		}
 
-		for (Map.Entry<Character, Integer> set : map.entrySet()) {
-			if (set.getValue() == 1) {
-				return s.indexOf(set.getKey());
+		for (char ch : s.toCharArray()) {
+			if (map.get(ch) == 1) {
+				return s.indexOf(ch);
 			}
-			System.out.println(set.getKey() + ":" + set.getValue());
+
 		}
 
 		return -1;
